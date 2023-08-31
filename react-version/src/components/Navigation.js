819e5555b38a6d2cbe9806ad43e2fc1e237ae6fc
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-08-31 12:52:11 -0400

    Convert HTML to JSX and import styles for React version

--- react-version/src/components/Navigation.js
+++ react-version/src/components/Navigation.js
@@ -1,8 +1,16 @@
 function Navigation() {
   return (
-    <h1>
-      I am the navigation component
-    </h1>
+    <nav>
+      <span className="nav--text">tweeter</span>
+      <div className="btn btn--scroll">
+        <a href="#" className="btn__text btn--borderless" title="Compose Tweet">
+          {/* Compose new tweet button */}
+          <span className="btn--bold">Write</span> a new tweet
+          <br />
+          <i className="btn__icon fas fa-angle-double-down"></i>
+        </a>
+      </div>
+    </nav>
   );
 }
 
