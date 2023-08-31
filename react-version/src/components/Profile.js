56745d86d92c253c7da323d736696d047304c99e
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-08-31 17:19:37 -0400

    Implement JSX variables (placeholders for now)

--- react-version/src/components/Profile.js
+++ react-version/src/components/Profile.js
@@ -2,11 +2,11 @@ function Profile() {
   return (
     <aside>
       <div className="profile">
-        <img className="profile__image" src="./profile-hex.png" />
+        <img className="profile__image" src={avatar} />
       </div>
       <br />
       <div className="profile__name">
-        <h2><span className="profile--bold">Amy</span> Mansell</h2>
+        <h2><span className="profile--bold">{firstName}</span> {lastName}</h2>
       </div>
     </aside>
   );
--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,8 +1,12 @@
+const tweets = {
+  1: "What are you humming about?"
+};
+
 function TweetForm() {
   return (
     <section className="newtweet">
       <form method="post" action="/tweets" className="newtweet__form">
-        <textarea className="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
+        <textarea className="form__textarea" name="text" placeholder={tweets[1]}></textarea>
         <input type="submit" value="Tweet" className="form__input" />
         <span className="form__counter">140</span>
       </form>
